da096fca28802ec6b3763a3d2cd91e5b8a183b93
Jason <152582224+JEX2009@users.noreply.github.com>
2025-11-18 15:26:12 -0600

    feat (catch)

---
+++ Inventario.UI/Controllers/ErrorController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+public class ErrorController : Controller
+{
+    public ActionResult NotFound()
+    {
+        Response.StatusCode = 404;
+        Response.TrySkipIisCustomErrors = true;
+        return View("NotFound");
+    }
+
+    public ActionResult ServerError()
+    {
+        Response.StatusCode = 500;
+        Response.TrySkipIisCustomErrors = true;
+        return View("ServerError");
+    }
+}
