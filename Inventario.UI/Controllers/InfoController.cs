fed288a63682a81c869073c3fc99df7da5a5d7eb
develasmen <develasmen@gmail.com>
2025-12-02 18:23:01 -0600

    Se Agregan vistas Genericas.

---
+++ Inventario.UI/Controllers/InfoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Web.Mvc;
+
+namespace Inventario.UI.Controllers
+{
+    public class InfoController : Controller
+    {
+        // GET: Info/Sucursales
+        [AllowAnonymous]
+        public ActionResult Sucursales()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult TerminosCondiciones()
+        {
+            return View();
+        }
+
+        // GET: Info/AcercaDe
+        [AllowAnonymous]
+        public ActionResult AcercaDe()
+        {
+            return View();
+        }
+    }
+}
